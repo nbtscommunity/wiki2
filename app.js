d22d0d184c27921576a5e652af2cc3da1e2dce7c
Aria Stewart <aredridel@nbtsc.org>
2013-12-10 23:30:35 -0700

    Start switching based on request method

--- app.js
+++ app.js
@@ -4,12 +4,19 @@ var url = require('url');
 var pathWalk = require('./pathwalk');
 
 require('http').createServer(function (req, res) {
-    req.resume(); // Passenger
     var u = url.parse(req.url);
-    if (req.cgiHeaders && req.cgiHeaders.SCRIPT_NAME) u.pathname = u.path = u.href = u.href.replace(new RegExp('^' + req.cgiHeaders.SCRIPT_NAME + '/'), '');
 
-    pathWalk.call(repo, 'HEAD', u.pathname)(function (err, blob) {
-        if (err) return res.end(err);
-        res.end(util.inspect(blob));
-    });
+    if (req.cgiHeaders) req.resume(); // Passenger is streams1. Kind-of.
+
+    if (req.method == 'GET') {
+
+        if (req.cgiHeaders && req.cgiHeaders.SCRIPT_NAME) u.pathname = u.path = u.href = u.href.replace(new RegExp('^' + req.cgiHeaders.SCRIPT_NAME + '/'), '');
+
+        pathWalk.call(repo, 'HEAD', u.pathname)(function (err, blob) {
+            if (err) return res.end(err);
+            res.end(util.inspect(blob));
+        });
+    } else {
+        res.end('method not supported');
+    }
 }).listen('passenger');
