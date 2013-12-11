367c35d1d14fd5c563eacd363cb16ae606beb830
Aria Stewart <aredridel@nbtsc.org>
2013-12-10 23:17:40 -0700

    Strip passenger base url if possible; use real path for file lookup

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 var repo = require('git-node').repo('pages.git');
 var util = require('util');
+var url = require('url');
 var pathWalk = require('./pathwalk');
 
 require('http').createServer(function (req, res) {
     req.resume(); // Passenger
-    pathWalk.call(repo, 'HEAD', '')(function (err, blob) {
+    var u = url.parse(req.url);
+    if (req.cgiHeaders && req.cgiHeaders.SCRIPT_NAME) u.pathname = u.path = u.href = u.href.replace(new RegExp('^' + req.cgiHeaders.SCRIPT_NAME + '/'), '');
+
+    pathWalk.call(repo, 'HEAD', u.pathname)(function (err, blob) {
         if (err) return res.end(err);
         res.end(util.inspect(blob));
     });
