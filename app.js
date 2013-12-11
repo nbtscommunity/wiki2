077fdfa97ee2407688d434cf9759fd67f0b02499
Aria Stewart <aredridel@nbtsc.org>
2013-12-10 23:42:33 -0700

    Render markdown

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var repo = require('git-node').repo('pages.git');
 var util = require('util');
 var url = require('url');
 var pathWalk = require('./pathwalk');
+var markdown = require( "markdown" ).markdown;
 
 require('http').createServer(function (req, res) {
     var u = url.parse(req.url);
@@ -9,12 +10,16 @@ require('http').createServer(function (req, res) {
     if (req.cgiHeaders) req.resume(); // Passenger is streams1. Kind-of.
 
     if (req.method == 'GET') {
-
         if (req.cgiHeaders && req.cgiHeaders.SCRIPT_NAME) u.pathname = u.path = u.href = u.href.replace(new RegExp('^' + req.cgiHeaders.SCRIPT_NAME + '/'), '');
 
         pathWalk.call(repo, 'HEAD', u.pathname)(function (err, blob) {
             if (err) return res.end(err);
-            res.end(util.inspect(blob));
+            if (blob.type != 'blob') {
+                res.end(util.inspect(blob));
+            } else {
+                res.setHeader('content-type', 'text/html');
+                res.end(markdown.toHTML(blob.body.toString()));
+            }
         });
     } else {
         res.end('method not supported');
